b2c6b04f0e45e0631483cc6313bd9ce9cd2c8856
its.ssskye <its.ssskyler@gmail.com>
2025-07-02 10:23:39 +0700

    Click animation

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 // Start click count at 0
 let clickCount = 0;
 
-//Grab button and counter elements from HTML
+// Grab button and counter elements from HTML
 const button = document.getElementById("chaos-button");
 const counterText = document.getElementById("counter");
 
-//Run every time button is clicked
+// Run every time button is clicked
 button.addEventListener("click", () => {
     clickCount++; // Add 1 to counter
     counterText.textContent = `Clicks: ${clickCount}`; // Update counter text
+
+    // Add class that triggers click animation
+    button.classList.add("shrink-effect");
+
+    // Remove class shortly after to complete animation
+    setTimeout(() => {
+        button.classList.remove("shrink-effect");
+    }, 200); // Match animation durations
 });
